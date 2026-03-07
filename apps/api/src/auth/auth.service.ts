df72602f8eaa83b416255db5f08b9b4a001e57b4
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-07 17:25:05 +0530

    feat(auth): create login method and dto

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(dto: RegisterDto) {
     const existingUser = await this.prisma.user.findUnique({
@@ -29,6 +33,28 @@ export class AuthService {
     return {
       message: 'User created',
       userId: user.id,
+    };
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user || !user.password) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const token = this.jwtService.sign({ userId: user.id, email: user.email });
+
+    return {
+      accessToken: token,
     };
   }
 }
