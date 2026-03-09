afdabd03dcb13fd1b947dc9413e46afc30030b96
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-09 15:34:31 +0530

    feat: create Comment DTO and API

--- apps/api/src/comments/comments.controller.ts
+++ apps/api/src/comments/comments.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
-@Controller('comments')
-export class CommentsController {}
+@Controller('tasks/:taskId/comments')
+export class CommentsController {
+  constructor(private commentsService: CommentsService) {}
+
+  @Post()
+  create(@Param('taskId') taskId: string, @Body() dto: CreateCommentDto) {
+    const userId = 'USER_ID'; // replace later with auth user
+    return this.commentsService.createComment(taskId, userId, dto);
+  }
+}
--- apps/api/src/comments/comments.service.ts
+++ apps/api/src/comments/comments.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(private prisma: PrismaService) {}
+
+  async createComment(taskId: string, userId: string, dto: CreateCommentDto) {
+    return this.prisma.comment.create({
+      data: {
+        content: dto.content,
+        taskId,
+        userId,
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
+}
