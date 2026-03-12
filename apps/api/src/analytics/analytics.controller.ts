8a2815c9b07ef62305d13cbd335236c3071d09a2
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-12 12:06:48 +0530

    feat: add project analytics endpoint

--- apps/api/src/analytics/analytics.controller.ts
+++ apps/api/src/analytics/analytics.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { AnalyticsService } from './analytics.service';
 
 @Controller('analytics')
-export class AnalyticsController {}
+export class AnalyticsController {
+  constructor(private analyticsService: AnalyticsService) {}
+
+  @Get('projects/:projectId/metrics')
+  getProjectMetrics(@Param('projectId') projectId: string) {
+    return this.analyticsService.getProjectMetrics(projectId);
+  }
+}
