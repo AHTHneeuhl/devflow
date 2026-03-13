aa6a9c177c9c95021416161af7805e035e0486ce
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-13 13:10:51 +0530

    feat(observability): add http request duration metric

--- apps/api/src/observability/observability.service.ts
+++ apps/api/src/observability/observability.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { collectDefaultMetrics, Counter } from 'prom-client';
+import { collectDefaultMetrics, Counter, Histogram } from 'prom-client';
 
 @Injectable()
 export class ObservabilityService {
   httpRequests = new Counter({
     name: 'http_requests_total',
     help: 'Total HTTP requests',
+  });
+
+  httpRequestDuration = new Histogram({
+    name: 'http_request_duration_ms',
+    help: 'HTTP request duration in ms',
+    buckets: [50, 100, 200, 500, 1000],
   });
 
   constructor() {
