e8c36e354f6ca1051a21d2ab3b3a5d9ef51f37ab
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-13 13:14:24 +0530

    feat(observability): enable terminus module

--- apps/api/src/observability/observability.module.ts
+++ apps/api/src/observability/observability.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { MetricsController } from './metrics.controller';
 import { ObservabilityController } from './observability.controller';
 import { ObservabilityService } from './observability.service';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [MetricsController, ObservabilityController],
   providers: [ObservabilityService],
 })
