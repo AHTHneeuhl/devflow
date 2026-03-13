fa2354c898b40bb93b556f5012081439d6303ed9
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-03-13 14:16:23 +0530

    feat(billing): create billing controller

--- apps/api/src/billing/billing.module.ts
+++ apps/api/src/billing/billing.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StripeService } from './stripe/stripe.service';
+import { BillingController } from './billing.controller';
 
 @Module({
   providers: [StripeService],
   exports: [StripeService],
+  controllers: [BillingController],
 })
 export class BillingModule {}
